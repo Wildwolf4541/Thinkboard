59103879209a8b401b500c3bdaa8cbca0a01d62d
Wildwolf4541 <guptakhil05@gmail.com>
2026-01-26 02:38:46 +0530

    Final Production Changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,36 @@
 import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
+import path from 'path'
 
 import notesRoutes from './routes/notesRoutes.js'
 import { connectDB } from './config/db.js'
-import dotenv from 'dotenv'
 
-const app = express()
-
-app.use(cors({
-    origin: 'http://localhost:5173'
-}
-)) // ✅ Allow all origins
-
-app.use(express.json()) // To parse JSON bodies
 dotenv.config()
-
+const app = express()
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve()
+
+// middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
+app.use(express.urlencoded({ extended: true })) // To parse URL-encoded bodies
+app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 connectDB().then(()=>{
     app.listen(PORT, () => {
         console.log("Server is running on Port:", PORT);
