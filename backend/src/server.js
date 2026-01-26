69e2cd90ebf981fbf7fe8851de86ad70b98d4e30
Wildwolf4541 <guptakhil05@gmail.com>
2026-01-26 18:33:33 +0530

    Fixed start eror

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,16 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import path from 'path'
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
 
-import notesRoutes from './routes/notesRoutes.js'
-import { connectDB } from './config/db.js'
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
 
-dotenv.config()
-const app = express()
-const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve()
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // middleware
 if (process.env.NODE_ENV !== "production") {
@@ -20,28 +21,28 @@ if (process.env.NODE_ENV !== "production") {
   );
 }
 app.use(express.json()); // this middleware will parse JSON bodies: req.body
-app.use(express.urlencoded({ extended: true })) // To parse URL-encoded bodies
-app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
+// our simple custom middleware
+// app.use((req, res, next) => {
+//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//   next();
+// });
+
+app.use("/api/notes", notesRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  app.use((req, res) => {
+    res.sendFile(
+      path.join(__dirname, "../frontend", "dist", "index.html")
+    );
   });
 }
+// start server & connect to DB
 
-connectDB().then(()=>{
-    app.listen(PORT, () => {
-        console.log("Server is running on Port:", PORT);
-    })
-})
-
-//Middleware
-// app.use((req,res,next)=>{
-//     console.log(`${req.method} request received at ${req.url} `);
-//     next();
-// })
-
-// An endpoint is a communication point between client and server URL + HTTP Method.
-app.use('/api/notes',notesRoutes)
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  });
+});
